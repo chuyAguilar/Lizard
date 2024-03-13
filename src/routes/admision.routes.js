9c845bbf8ef13c574e3ae2d9687c001eb9886af3
chuy <jesusaguilar32265@gmail.com>
2024-03-13 15:17:02 -0600

    protegi la pinches rutas

--- src/routes/admision.routes.js
+++ src/routes/admision.routes.js
@@ -2,13 +2,35 @@ import { Router } from "express";
 
 const router = Router();
 
+/* 
+  #####                                                                       
+ #     #  ####  #    # ##### #####   ####  #      #      ###### #####   ####  
+ #       #    # ##   #   #   #    # #    # #      #      #      #    # #      
+ #       #    # # #  #   #   #    # #    # #      #      #####  #    #  ####  
+ #       #    # #  # #   #   #####  #    # #      #      #      #####       # 
+ #     # #    # #   ##   #   #   #  #    # #      #      #      #   #  #    # 
+  #####   ####  #    #   #   #    #  ####  ###### ###### ###### #    #  ####  
+                                                                              
+*/
 import * as admisionCtrl from '../controllers/admision.controller.js';
 
+/*
+ #     #                                                                  
+ ##   ## # #####  #####  #      ###### #    #   ##   #####  ######  ####  
+ # # # # # #    # #    # #      #      #    #  #  #  #    # #      #      
+ #  #  # # #    # #    # #      #####  #    # #    # #    # #####   ####  
+ #     # # #    # #    # #      #      # ## # ###### #####  #           # 
+ #     # # #    # #    # #      #      ##  ## #    # #   #  #      #    # 
+ #     # # #####  #####  ###### ###### #    # #    # #    # ######  ####  
+*/
+import { authJwt } from "../middlewares/index.js";
+
+
 //Establecer ruta admision mediante el metodo GET
-router.get('/', admisionCtrl.getAdmisiones);
-router.get('/:admisionId', admisionCtrl.getAdmisionById);
-router.post('/', admisionCtrl.createAdmision);
-router.put('/:admisionId', admisionCtrl.updateAdmision);
-router.delete('/:admisionId', admisionCtrl.deleteAdmision);
+router.get('/',[authJwt.verifyToken, authJwt.isCostumer], authJwt.verifyToken,admisionCtrl.getAdmisiones);
+router.get('/:admisionId', [authJwt.verifyToken, authJwt.isCostumer],admisionCtrl.getAdmisionById);
+router.post('/',[authJwt.verifyToken, authJwt.isAdmin], admisionCtrl.createAdmision);
+router.put('/:admisionId', [authJwt.verifyToken, authJwt.isAdmin],admisionCtrl.updateAdmision);
+router.delete('/:admisionId',[authJwt.verifyToken, authJwt.isAdmin], admisionCtrl.deleteAdmision);
 
 export default router;
