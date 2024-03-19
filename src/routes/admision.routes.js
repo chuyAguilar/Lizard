82468e49b7041075fe402fcb05cf3b8a8d5d65f8
Ninis <MiguelAdolfoV@outlook.com>
2024-03-19 17:48:43 -0600

    ya no dice costumer

--- src/routes/admision.routes.js
+++ src/routes/admision.routes.js
@@ -27,8 +27,8 @@ import { authJwt } from "../middlewares/index.js";
 
 
 //Establecer ruta admision mediante el metodo GET
-router.get('/',[authJwt.verifyToken, authJwt.isCostumer], authJwt.verifyToken,admisionCtrl.getAdmisiones);
-router.get('/:admisionId', [authJwt.verifyToken, authJwt.isCostumer],admisionCtrl.getAdmisionById);
+router.get('/',[authJwt.verifyToken, authJwt.isCustomer], authJwt.verifyToken,admisionCtrl.getAdmisiones);
+router.get('/:admisionId', [authJwt.verifyToken, authJwt.isCustomer],admisionCtrl.getAdmisionById);
 router.post('/',[authJwt.verifyToken, authJwt.isAdmin], admisionCtrl.createAdmision);
 router.put('/:admisionId', [authJwt.verifyToken, authJwt.isAdmin],admisionCtrl.updateAdmision);
 router.delete('/:admisionId',[authJwt.verifyToken, authJwt.isAdmin], admisionCtrl.deleteAdmision);
