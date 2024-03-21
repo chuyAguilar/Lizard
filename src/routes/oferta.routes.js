0ed5889cec02bc093d5a7a516a6ace74b9b7401b
Aura Renata <perezugaldeaurarenata@gmail.com>
2024-03-21 13:24:49 -0600

    resolviendo conflictos 2 en oferta routes

--- src/routes/oferta.routes.js
+++ src/routes/oferta.routes.js
@@ -28,8 +28,7 @@ import { authJwt } from "../middlewares/index.js";
 
 //router.get('/', [authJwt.verifyToken,authJwt.isCostumer], ofertaCtrl.getOfertas);
 
-router.get('/', ofertaCtrl.getOfertas);
-router.get('/:ofertaId',[authJwt.verifyToken,authJwt.isCostumer],  ofertaCtrl.getOfertaById);
+
 router.post('/',[authJwt.verifyToken,authJwt.isAdmin], ofertaCtrl.createOferta);
 router.put('/:ofertaId',[authJwt.verifyToken,authJwt.isAdmin],  ofertaCtrl.updateOferta);
 router.delete('/:ofertaId', [authJwt.verifyToken,authJwt.isAdmin], ofertaCtrl.deleteOferta);
