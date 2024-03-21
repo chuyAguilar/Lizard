97f6163a45aceaae0db42a6946d95dd866e31ce0
Aura Renata <perezugaldeaurarenata@gmail.com>
2024-03-21 00:44:56 -0600

    Second commit

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -3,35 +3,40 @@ import React, { Component } from "react";
 class Admin extends Component {
   render() {
     return (
-      <div>
-        <h3>SPA App - About</h3>
-        <p>This is a paragraph on the About of the SPA App.</p>
-        <p>The Team of SPA App.</p>
-        <table>
-        <thead>
-            <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
+      <div class="p-4 bg-gray-100 rounded-lg shadow-lg">
+        <h3 class="text-lg font-semibold mb-2">Lizard App - Administrador</h3>
+        <p class="mb-4">Esta seccion es para el administrador</p>
+        <table class="w-full border-collapse border border-gray-200">
+          <thead>
+            <tr class="bg-gray-200">
+              <th class="border border-gray-300 px-4 py-2">ID</th>
+              <th class="border border-gray-300 px-4 py-2">Name</th>
+              <th class="border border-gray-300 px-4 py-2">Email</th>
             </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <td>1</td>
-            <td>John Doe</td>
-            <td>johndoe@example.com</td>
+          </thead>
+          <tbody>
+            <tr class="border-t border-gray-200">
+              <td class="border border-gray-300 px-4 py-2">1</td>
+              <td class="border border-gray-300 px-4 py-2">John Doe</td>
+              <td class="border border-gray-300 px-4 py-2">
+                johndoe@example.com
+              </td>
             </tr>
-            <tr>
-            <td>2</td>
-            <td>Jane Doe</td>
-            <td>janedoe@example.com</td>
+            <tr class="border-t border-gray-200">
+              <td class="border border-gray-300 px-4 py-2">2</td>
+              <td class="border border-gray-300 px-4 py-2">Jane Doe</td>
+              <td class="border border-gray-300 px-4 py-2">
+                janedoe@example.com
+              </td>
             </tr>
-            <tr>
-            <td>3</td>
-            <td>Bob Smith</td>
-            <td>bobsmith@example.com</td>
+            <tr class="border-t border-gray-200">
+              <td class="border border-gray-300 px-4 py-2">3</td>
+              <td class="border border-gray-300 px-4 py-2">Bob Smith</td>
+              <td class="border border-gray-300 px-4 py-2">
+                bobsmith@example.com
+              </td>
             </tr>
-        </tbody>
+          </tbody>
         </table>
       </div>
     );
